2a998320c62e362bcda6e474463946088d47afcc
Andrew Aubury <andrewaubury1@gmail.com>
2018-03-21 09:47:54 +0000

    AI Command DialogFlow

--- src/main/java/me/piggypiglet/minepos/bot/commands/info/Oof.java
+++ src/main/java/me/piggypiglet/minepos/bot/commands/info/Oof.java
@@ -1,15 +1,7 @@
 package me.piggypiglet.minepos.bot.commands.info;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.dialogflow.v2beta1.Dialogflow;
-import com.google.api.services.dialogflow.v2beta1.DialogflowRequest;
+
 import me.piggypiglet.minepos.bot.core.framework.Command;
-import me.piggypiglet.minepos.bot.core.handlers.CommandHandler;
-import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 // ------------------------------
